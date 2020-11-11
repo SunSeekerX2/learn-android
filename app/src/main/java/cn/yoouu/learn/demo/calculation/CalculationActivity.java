3aa4f46d2acdc90b495fe4480edcfa11780b6f9f
SunSeekerX <1647800606@qq.com>
2020-11-11 23:29:05 +0800

    feat：update

---
+++ app/src/main/java/cn/yoouu/learn/demo/calculation/CalculationActivity.java
@@ -0,0 +1,16 @@
+package cn.yoouu.learn.demo.calculation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cn.yoouu.learn.R;
+
+public class CalculationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculation);
+    }
+}
